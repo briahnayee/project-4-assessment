acef42edcf0a0ad11f21a695865c5774a07007b8
Bri Y <bri@bris-air.attlocal.net>
2020-06-02 15:00:53 -0700

    Updates

---
+++ precache-manifest.2658e325abdee25b42ec1384c0d20b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78523e0271ec28d6b0ddb48a3dfdc900",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "243507d9945c4da0d9b0",
+    "url": "/project-4-assessment/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "b9fc516e90ce04ccefe5",
+    "url": "/project-4-assessment/static/js/2.d1616215.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.d1616215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243507d9945c4da0d9b0",
+    "url": "/project-4-assessment/static/js/main.99a148f9.chunk.js"
+  },
+  {
+    "revision": "f6594897fedc8d6cccb8",
+    "url": "/project-4-assessment/static/js/runtime-main.911a2f41.js"
+  }
+]);
